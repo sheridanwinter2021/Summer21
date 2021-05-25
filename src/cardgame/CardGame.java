2e7f903a85949cad23aab1cf80e977af8e57a094
sivagama <sivagama.srinivasan@sheridanc.on.ca>
2021-05-25 10:40:42 -0400

    week 2

---
+++ src/cardgame/CardGame.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cardgame;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author sivagamasrinivasan
+ */
+public class CardGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Card[] deck = new Card[4];
+        Scanner in = new Scanner(System.in);
+        System.out.println(" enter your card values: ");
+        for ( int i=0;i<deck.length;i++)
+        {
+            deck[i]= new Card(in.next(),in.nextInt());            
+        }
+    System.out.println(" your card values are: ");
+        for ( int i=0;i<deck.length;i++)
+        {
+            System.out.println(deck[i].getSuit() +" " +  deck[i].getValue());
+        }
+    }
+    
+}
